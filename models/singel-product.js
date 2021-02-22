5a6b0b5979e0ca216aa711fbc53574347a14948c
morteza roozbehi <mr.roz16126@gmail.com>
2021-02-22 06:58:53 +0330

    noting

--- models/singel-product.js
+++ models/singel-product.js
@@ -3,12 +3,13 @@ const products = []
 module.exports = class Product {
 
     constructor(title, descerption, price) {
-        this.title = title
-        this.descerption = descerption
-        this.price = price
-        this.id = Math.floor(Math.random() * 10)
+            this.title = title
+            this.descerption = descerption
+            this.price = price
+            this.id = Math.floor(Math.random() * 10)
 
-    }
+        }
+        /////
 
     saveProduct() {
 
